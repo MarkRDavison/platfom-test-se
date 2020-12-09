cf4909455e29f38cb86a47c883c0d851e9044fe3
Preet X Sangha <preet.sangha@serko.com>
2020-12-09 19:13:32 +1300

    Added the instructions

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # Software Engineer in Platform code test
 
-This code is an example of a very .net application that implements a web app what talks to a web api and through to a DB. It's purpose is to present some part of an application that has profiles which information that the supposed app would record about a person - ie. Name, Address, email etc. The developer is no longer here and we need to complete the work to release this.
+This code is an example of a very .net basic application that implements a web app what talks to a web api and on to a DB. Its purpose is to present some part of an application that has user profiles which is information that the supposed app would record about a person - ie. Name, Address, Email etc. The developer is no longer here and we need to complete the work to release this.
 
-This code is very basic and is in a very poor state and not something we can release as is.
+This code is very basic state and not something we can release as is.
 
 ## Challenge
 
 - How would you improve the code, so that we can **ship it** to our first customers?
 - What is wrong with it?
 - What is missing?
-- What should be cleaned up?  
+- What should be cleaned up? 
+- We will be supporting this for a long time so we need to ensure that the code is in a state that this can be acheived. What will we need to add/change to make that sure we can?
 
-We will be supporting this for a long time so we need to ensure that the code is in a state that this can be acheived.
 ## Requirements
 
 - This test needs .net core 3.1+ to run which you can download from [here](https://dotnet.microsoft.com/download).
 
 ### To build the code use the command `dotnet build`
-
+---
 e.g.
 
 ```
@@ -42,3 +42,36 @@ Time Elapsed 00:00:01.10
 C:\dev\test on  master[?!]
 
  ```
+
+### Deployment Infrastrastructure 
+
+---
+**NOTE** You'll need [Docker Desktop](https://www.docker.com/products/docker-desktop) installed to make this work. 
+
+There is a script (`init.ps1`) that can be used to create an apppropriate infrstructure.
+
+```
+.\init.ps1 -subscriptionName "your_sub" -name "your_name" -sqlPassword (ConvertTo-SecureString  -AsPlainText -Force "your_sql_password")
+```
+
+The following will be created:
+
+- A resource group (*d-aue-quasar-interviewtest-wilfred-rg*)
+- A Sql Server (*d-aue-quasar-interviewtest-wilfred-sql*)
+- A Sql DB (*d-aue-quasar-interviewtest-wilfred-db (d-aue-quasar-interviewtest-wilfred-sql/d-aue-quasar-interviewtest-wilfred-db) with credentials your_name-admin/your_sql_password*)
+- A Container registry containing on repository with the [latest DockerHub image of grafana](https://hub.docker.com/r/grafana/grafana/)
+- A App Service Plan (*d-aue-quasar-interviewtest-wilfred-asplan*)
+- A Web App Service (*d-aue-quasar-interviewtest-wilfred-asplan-app*)
+
+One manual step remaims and that is to set the deployment credentials
+```
+az webapp deployment user set --user-name "some_user_id" --password "some_user_deployment_password"
+```
+To deploy your app to the infrastructure you'll need to add an addition git remote to your repo.  The instructions are printed at the end of the script. e.g.:
+
+```
+git remote add azure "https://some_user_id@d-aue-quasar-interviewtest-your_name-asplan-app.scm.azurewebsites.net/d-aue-quasar-interviewtest-your_name-asplan-app.git"
+git push azure
+```
+
+You will supply those deployment credential when you push your app the first time. 
---
+++ init.ps1
@@ -0,0 +1,213 @@
+param (
+    [parameter(Mandatory=$false)] 
+    $force = $false,
+
+    [parameter(Mandatory=$true)] 
+    $subscriptionName = "testsub",
+
+    [parameter(Mandatory=$false)] 
+    $name = "test2",
+
+    [parameter(Mandatory=$false)] 
+    $owner = "preet",
+
+    [parameter(Mandatory=$true)] 
+    [SecureString] $sqlPassword
+)
+
+$subscriptionName = "pulsars"
+
+# login and set subscription
+az login
+az account set --subscription $subscriptionName
+
+$location= "AustraliaEast"
+$namePrefix = "d-aue-quasar-interviewtest"
+
+$resourceGroupName = "$namePrefix-$name-rg"
+
+$sqlServerName = "$namePrefix-$name-sql"
+$sqlUsername = "$name-admin"
+
+$sqlDbName = "$namePrefix-$name-db"
+
+$acrName = "$namePrefix-$name-acr" -replace "-", ""
+
+$appServicePlanName = "$namePrefix-$name-asplan" 
+$webAppName = "$appServicePlanName-app" 
+
+function CreateWebapp()
+{
+    Write-Output "Webapp $webAppName"
+
+    $resource = (az webapp show --name $webAppName --resource-group $resourceGroupName )
+    if ($force -and $resource) {
+        Write-Output "exists. deleting"
+        az webapp delete --name $webAppName  --resource-group $resourceGroupName  
+        $resource = $null
+    } 
+
+    if (!$resource) {
+        Write-Output "doesn't exist. creating"
+        # need the '"" in that form as powershell interprets the | char
+        $resource = (az webapp create --name $webAppName --resource-group $resourceGroupName  --plan $appServicePlanName --deployment-local-git --runtime '"DOTNETCORE|3.1"')
+    }
+
+    $dbConnectionString = (az sql db show-connection-string --client ado.net --server $sqlServerName --name "MyDbConnection")
+    $dbConnectionString = $dbConnectionString -replace "<username>", $sqlUsername  
+    $dbConnectionString = $dbConnectionString -replace "<password>", $sqlPassword  
+    az webapp config connection-string set --resource-group $resourceGroupName --name $webAppName --settings MyDbConnection=$dbConnectionString --connection-string-type SQLAzure
+
+    $gitUrl = (az webapp deployment source config-local-git --resource-group $resourceGroupName --name $webAppName --query url)
+
+    # this is set at the subscription level
+    #az webapp deployment user set --user-name "..." --password "...."
+
+    Write-Output $resource    
+    
+     Write-Output "Add the following remote to your applictions and push" 
+    Write-Output "\n"
+    Write-Output "git remote add azure $gitUrl"
+    Write-Output "git push azure"    
+}
+
+function CreateAppServicePlan()
+{
+    Write-Output "App Service Plan $appServicePlanName"
+
+    $resource = (az appservice plan show --name $appServicePlanName --resource-group $resourceGroupName )
+    if ($force -and $resource) {
+        Write-Output "exists. deleting"
+        az appservice plan delete --name $appServicePlanName  --resource-group $resourceGroupName
+        $resource = $null
+    } 
+
+    if (!$resource) {
+        Write-Output "doesn't exist. creating"
+        $resource = (az appservice plan create --name $appServicePlanName --resource-group $resourceGroupName --sku "F1" --location $location)
+    }
+
+    Write-Output $resource
+}
+
+function UploadGrafanaImage()
+{
+    Write-Output "Upload Grafana Instance to $acrName"
+
+    $dockerHubImageName='grafana/grafana'
+    docker image pull $dockerHubImageName
+
+    $acrloginserver=(az acr show --name $acrName --query loginServer)
+    write-Output "Login server $acrloginserver"
+
+    $acrUsername = (az acr credential show -n  dauequasarinterviewtesttest2acr --query "username")
+    $acrPw = (az acr credential show -n  dauequasarinterviewtesttest2acr --query "passwords[0].value")
+     
+    $acrImageName="$acrloginserver/grafana:v1"
+    Write-Output "Acr Image Name: $acrImageName"
+    
+    Write-Output "Uploading image"
+    docker tag $dockerHubImageName $acrImageName
+    az acr login --name $acrName --username $acrUsername --password $acrPw
+    docker push $acrImageName
+
+    # keep these here for debuging in future
+    # az acr repository list --name $acrName
+    # az acr repository show-tags --name $acrName --repository "grafana"
+
+    Write-Output "Acr Image Name: $acrImageName"
+}
+
+
+function CreateContainerRegistry()
+{
+    Write-Output "Container Registry $acrName"
+
+    $resource = (az acr show --name $acrName)
+    if ($force -and $resource) {
+        Write-Output "exists. deleting"
+        az acr delete --name $acrName  --resource-group $resourceGroupName
+        $resource = $null
+    } 
+
+    if (!$resource) {
+        Write-Output "doesn't exist. creating"
+        $resource = (az acr create --name $acrName  --resource-group $resourceGroupName --sku Basic --admin-enabled)
+    }
+
+    Write-Output $resource
+}
+
+
+function AddSqlFirewallRule()
+{
+    $myClientIp = ((Invoke-WebRequest -uri "http://ifconfig.me/ip").Content)
+    Write-Output "Adding Client Ip Address $myClientIp to Sql Server Firewall"
+    az sql server firewall-rule create --name "$env:ComputerName." --server $sqlServerName --resource-group $resourceGroupName --start-ip-address=$myClientIp --end-ip-address=$myClientIp
+}
+
+function CreateSqlDb()
+{
+    Write-Output "Sql Db $sqlDbName"
+
+    $resource = (az sql db show --name $sqlDbName --server $sqlServerName --resource-group $resourceGroupName )
+    if ($force -and $resource) {
+        Write-Output "exists. deleting"
+        az sql db delete --name $sqlDbName  --server $sqlServerName  --resource-group $resourceGroupName
+        $resource = $null
+    } 
+
+    if (!$resource) {
+        Write-Output "doesn't exist. creating"
+        $resource = (az sql db create --name $sqlDbName  --server $sqlServerName  --resource-group $resourceGroupName --family Gen5 --edition "GeneralPurpose" --compute-model "Serverless" --capacity 1 --max-size "1GB")
+    }
+
+    Write-Output $resource
+}
+
+function CreateSqlServer()
+{
+    Write-Output "Sql Db $sqlServerName"
+
+    $resource = (az sql server show --name $sqlServerName --resource-group $resourceGroupName )
+    if ($force -and $resource) {
+        Write-Output "exists. deleting"
+        az sql server delete --name $sqlServerName  --resource-group $resourceGroupName
+        $resource = $null
+    } 
+
+    if (!$resource) {
+        Write-Output "doesn't exist. creating"
+        $resource = (az sql server create --name $sqlServerName --resource-group $resourceGroupName --admin-password $sqlPassword --admin-user $sqlUsername --assign-identity --location $location)
+    }
+
+    Write-Output $resource
+}
+
+function CreateResourceGroup()
+{
+    Write-Output "Resource Group $resourceGroupName"
+
+    $resourceGroup = (az group show --name $resourceGroupName)
+    if ($force -and $resourceGroup) {
+        Write-Output "exists. deleting"
+        az group delete --name $resourceGroupName
+        $resourceGroup = $null
+    } 
+
+    if (!$resourceGroup) {
+        Write-Output " doesn't exist. creating"
+        $resourceGroup = (az group create --name $resourceGroupName --tags Owner=$owner --location $location)
+    }
+
+    Write-Output $resourceGroup
+}
+
+CreateResourceGroup
+CreateSqlServer
+CreateSqlDb
+AddSqlFirewallRule
+CreateContainerRegistry
+UploadGrafanaImage
+CreateAppServicePlan
+CreateWebapp
