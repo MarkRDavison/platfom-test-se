085fef2dd6793460bba32663965e7cf59f4f8cf7
Mark Davison <markdavison0+github@gmail.com>
2021-02-25 17:29:02 +1300

    run migrations on startup if needed

--- ProfilesApp/Startup.cs
+++ ProfilesApp/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using DotNetCoreSqlDb.Models;
+using System.Linq;
 
 namespace DotNetCoreSqlDb
 {
@@ -27,12 +28,14 @@ namespace DotNetCoreSqlDb
                 connectionString = Configuration.GetConnectionString("MySqlLiteConnection");
                 services.AddDbContext<MyDatabaseContext>(options => options.UseSqlite(connectionString));
             } else {
-                services.AddDbContext<MyDatabaseContext>(options => options.UseSqlServer(connectionString));
+                services.AddDbContext<MyDatabaseContext>(options => {
+                    options.UseSqlServer(connectionString);
+                });
             }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MyDatabaseContext db)
         {
             if (env.IsDevelopment())
             {
@@ -44,6 +47,8 @@ namespace DotNetCoreSqlDb
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //app.UseHsts();
             }
+
+            applyMigrations(db);
 
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
@@ -58,6 +63,17 @@ namespace DotNetCoreSqlDb
                     name: "default",
                     pattern: "{controller=Profiles}/{action=Index}/{id?}");
             });
+        }
+
+        private static void applyMigrations(MyDatabaseContext db)
+        {
+            var pending = db.Database.GetPendingMigrations().ToList();
+            var applied = db.Database.GetAppliedMigrations().ToList();
+            var needsApplying = pending.Where(p => !applied.Contains(p)).ToList();
+            if (needsApplying.Any())
+            {
+                db.Database.Migrate();
+            }
         }
     }
 }
